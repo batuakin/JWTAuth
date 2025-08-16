44df2b7c14b67d7693a7af13442deca8ce434ad9
batuakin <batuhanakin71@gmail.com>
2025-08-16 13:54:05 +0300

    Add project files.

---
+++ JWTAuthentication.Core/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Core.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ JWTAuthentication.Core/Repositories/Abstract/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Core.Repositories.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IQueryable<T>> Where(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        T Update(T entity);
+
+    }
+}
